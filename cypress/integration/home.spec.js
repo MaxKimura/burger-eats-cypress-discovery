a3f93fb8256387f1ffe0dc82638761adfb2c3bda
Maxwell Allan da Silva Kimura <maxwell.kimura@softplan.com.br>
2022-07-06 23:14:22 -0300

    ajuste limpando dados em excesso

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -3,9 +3,7 @@
 // describe é a suite de testes, it é o caso de teste
 describe('home page', ()=>{
     it ('app deve estar online', ()=>{
-
-        cy.viewport(1440, 900)
-        cy.visit('https://buger-eats.vercel.app')
+        cy.visit('/')
         cy.get('#page-home main h1').should('have.text', 'Seja um parceiro entregador pela Buger Eats')
 
     })
